337866ff92ef055192db246f13660e6f0c3d94c2
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2025-04-21 16:05:06 +0200

    Tmp rmv
    Signed-off-by: Juan Manuel Leflet Estrada <jleflete@redhat.com>

--- default/generated/spring-boot/tests/data/core-changes/src/main/java/org/konveyor/core/ApplicationProperties.java
+++
@@ -1,9 +0,0 @@
-package org.konveyor.core;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
-
-@ConfigurationProperties(prefix = "example")
-@ConstructorBinding
-public class ApplicationProperties {
-}
--- default/generated/spring-framework/tests/data/web-applications/src/main/java/org/konveyor/controller/NonDetectableController.java
+++
@@ -1,7 +0,0 @@
-package org.konveyor.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping
-public class NonDetectableController {
-}
